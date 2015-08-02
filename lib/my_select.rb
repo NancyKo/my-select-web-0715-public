a2470fa24c31e0e4c3c5fc42e3b1c71b2e03cae8
NancyKo <konancys@gmail.com>
2015-08-02 12:51:56 -0400

    Took a while but now complete

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,16 @@
+require 'pry'
 def my_select(collection)
-  puts "This block should not run!" if collection.size == []
-  if collection != 0
-    collection.find_all { |e| e.even? }  
+  puts "This block should not run!" if collection.empty?
+  new_collection = []
+  counter = 0
+  while collection.size > counter
+    # if the code block is true for such index, then push that element into new_collection
+   # we don't need to write the block, code block is given in spec
+    new_collection.push(collection[counter] ) if yield collection[counter]
+    counter += 1
+    # binding.pry
   end
-end
+  new_collection
+end 
+
+# my_select([1, 2, 3, 4, 5] )
